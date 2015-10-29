eaedfdabff74cdce33630fc6594bacdf73f724b3
Fatih Eraslan <era14331@spengergasse.at>
2015-10-29 13:45:50 +0100

    commit

--- anzeige.php
+++ anzeige.php
@@ -5,12 +5,6 @@
    <BODY>
        <div class="loader"></div>
    </BODY>
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript">
-$(window).load(function() {
-	$(".loader").fadeOut("slow");
-})
-</script>
 </HTML>
 <?php
         session_start();
